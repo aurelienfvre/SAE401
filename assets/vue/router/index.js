ce50fc871a02e58ec5a691eaed55d51ba74caf15
khciela <celiakhiev@email.com>
2024-03-29 15:01:01 +0100

    router

---
+++ assets/vue/router/index.js
@@ -0,0 +1,48 @@
+import { createRouter, createWebHistory } from "vue-router";
+const BASE_URL = process.env.BASE_URL || '/';
+
+const router = createRouter({
+    history: createWebHistory(BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "Home",
+            component: () => import("../views/Home.vue"),
+        },
+        {
+            path: "/about",
+            name: "About",
+            component: () => import("../views/About.vue"),
+        },
+        {
+            path: "/education",
+            name: "Education",
+            component: () => import("../views/Education.vue"),
+        },
+        {
+            path: "/contact",
+            name: "Contact",
+            component: () => import("../views/Contact.vue"),
+        },
+        {
+            path: "/login",
+            name: "Login",
+            component: () => import("../views/Login.vue"),
+        },
+        {
+            path: "/register",
+            name: "Register",
+            component: () => import("../views/Register.vue"),
+        },
+        {
+            path: "/legal",
+            name: "Legal",
+            component: () => import("../views/Legal.vue"),
+        },
+    ],
+    scrollBehavior(to, from, savedPosition) {
+        return { top: 0, behavior: "smooth" };
+    },
+});
+
+export default router;
