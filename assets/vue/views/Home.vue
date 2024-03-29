a8798a5ee4b130df5cd5a7fabb1e7a5944c5d20d
khciela <celiakhiev@email.com>
2024-03-29 14:39:49 +0100

    initialise home

---
+++ assets/vue/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view />
+    <!-- Chaque composant de route s'affiche ici -->
+  </div>
+</template>
+
+<script>
+// Pas besoin d'importer CardContainer ici
+export default {
+  name: "App",
+};
+</script>
+<style>
+/* Vous pouvez définir le style de fond ici s'il est spécifique à une page */
+.content {
+  padding: 20px;
+  /* Ajoutez plus de styles pour votre contenu ici */
+}
+</style>
