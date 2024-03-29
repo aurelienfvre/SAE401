1ab9cc53268dee81c7c4d51f37792b9d620277bd
khciela <celiakhiev@email.com>
2024-03-29 14:49:31 +0100

    avancée legal

--- assets/vue/views/Legal.vue
+++ assets/vue/views/Legal.vue
@@ -28,6 +28,44 @@
         Le contenu fourni par EcoLearn est protégé par les lois sur la propriété
         intellectuelle et reste la propriété exclusive de ses créateurs.
       </p>
+
+      <h2 class="section-title">Protection des données personnelles</h2>
+      <p>
+        Nous prenons la confidentialité très au sérieux et nous engageons à
+        protéger les données personnelles de nos utilisateurs.
+      </p>
+
+      <h2 class="section-title">Sécurité</h2>
+      <p>
+        La sécurité de notre plateforme est une priorité absolue, garantissant
+        une expérience sûre pour tous nos utilisateurs.
+      </p>
+
+      <h2 class="section-title">Droit applicable</h2>
+      <p>
+        L'utilisation de notre site et de nos services est régie par le droit
+        français, indépendamment du lieu d'utilisation.
+      </p>
+
+      <h2 class="section-title">Limitation de responsabilité</h2>
+      <p>
+        En utilisant EcoLearn, les utilisateurs reconnaissent que notre équipe
+        ne peut être tenue responsable de tout usage inapproprié du contenu ou
+        des services proposés.
+      </p>
+
+      <h2 class="section-title">Mise à jour</h2>
+      <p>
+        Nous nous réservons le droit de mettre à jour ces mentions légales à
+        tout moment, selon l'évolution de notre plateforme et des
+        réglementations.
+      </p>
+
+      <h2 class="section-title">Données statistiques de suivi</h2>
+      <p>
+        Nous utilisons des données statistiques pour suivre et améliorer
+        continuellement l'expérience utilisateur sur notre plateforme.
+      </p>
     </CardPage>
   </CardContainer>
   <Footer />
