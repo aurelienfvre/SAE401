439c790f5c5258032216fd9c3cedd239648d3bbd
aurelienfvre <126667429+aurelienfvre@users.noreply.github.com>
2024-04-11 00:07:53 +0200

    home

---
+++ assets/vue/views/Home.vue
@@ -0,0 +1,197 @@
+<template>
+  <CardContainer height="160vh">
+    <div class="home-content">
+      <p class="title__home">{{ $t('home.explore') }}</p>
+    </div>
+    <div class="interactive-components-wrapper">
+      <HomeFirstGrid class="home-first-grid" />
+      <InteractiveButton class="interactive-button" />
+      <HomeSecondGrid class="home-second-grid" />
+    </div>
+    <div class="educational-sections-wrapper">
+      <p>
+        {{ $t('home.ecolearnIntro') }}
+        <span class="color-ocean">{{ $t('home.ecosystems.ocean') }}</span>,
+        <span class="color-jungle">{{ $t('home.ecosystems.jungle') }}</span>,
+        <span class="color-forest">{{ $t('home.ecosystems.forest') }}</span>,
+        <span class="color-desert">{{ $t('home.ecosystems.desert') }}</span>,
+        <span class="color-arctic">{{ $t('home.ecosystems.arctic') }}</span>.
+
+      </p>
+      <p>
+        {{ $t('home.learnFun') }}
+      </p>
+    </div>
+    <div class="play-button-wrapper">
+      <PlayButton />
+    </div>
+  </CardContainer>
+
+  <div class="cards-container">
+    <HomeCard
+        cardColor="card-color-ocean"
+        :imageSrc="imageOcean"
+        :title="$t('home.cards.ocean.title')"
+        :subtitle="$t('home.cards.ocean.subtitle')"
+        titleColor="#D1E2FD"
+        subtitleColor="#fff"
+        route="/ocean"
+        buttonColor="#D1E2FD"
+        :buttonText="$t('home.cards.ocean.button')"
+        textColor="black"
+    />
+    <HomeCard
+        cardColor="card-color-jungle"
+        :imageSrc="imageJungle"
+        :title="$t('home.cards.jungle.title')"
+        :subtitle="$t('home.cards.jungle.subtitle')"
+        titleColor="#C1DD13"
+        subtitleColor="#fff"
+        route="/jungle"
+        buttonColor="#ABF3DE"
+        :buttonText="$t('home.cards.jungle.button')"
+        textColor="black"
+    />
+    <HomeCard
+        cardColor="card-color-desert"
+        :imageSrc="imageDesert"
+        :title="$t('home.cards.desert.title')"
+        :subtitle="$t('home.cards.desert.subtitle')"
+        titleColor="#D88BCA"
+        subtitleColor="#000"
+        route="/desert"
+        buttonColor="#FFE5D8"
+        :buttonText="$t('home.cards.desert.button')"
+        textColor="black"
+    />
+    <HomeCard
+        cardColor="card-color-arctic"
+        :imageSrc="imageArctic"
+        :title="$t('home.cards.arctic.title')"
+        :subtitle="$t('home.cards.arctic.subtitle')"
+        titleColor="#3A7CA3"
+        subtitleColor="#000"
+        route="/arctic"
+        buttonColor="#EDF4FF"
+        :buttonText="$t('home.cards.arctic.button')"
+        textColor="black"
+    />
+  </div>
+
+  <Footer />
+</template>
+
+<script setup>
+import { useI18n } from 'vue-i18n';
+import CardContainer from "@/components/CardContainer.vue";
+import InteractiveButton from "@/components/InteractiveButton.vue";
+import HomeFirstGrid from "@/components/HomeFirstGrid.vue";
+import HomeSecondGrid from "@/components/HomeSecondGrid.vue";
+import PlayButton from "@/components/PlayButton.vue";
+import HomeCard from "@/components/HomeCard.vue";
+import Footer from "@/components/Footer.vue";
+
+const { t } = useI18n();
+
+import imageOcean from "@/assets/image-ocean.webp";
+import imageJungle from "@/assets/image-jungle.webp";
+import imageDesert from "@/assets/image-desert.webp";
+import imageArctic from "@/assets/image-artic.webp";
+
+</script>
+
+<style scoped>
+.home-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.interactive-components-wrapper {
+  display: flex;
+  align-items: start;
+  justify-content: flex-start;
+}
+
+.home-first-grid,
+.interactive-button,
+.home-second-grid {
+  min-width: 200px;
+  margin-right: 20px;
+}
+
+.home-second-grid {
+  margin-right: 140px;
+  position: relative;
+  bottom: 5%;
+}
+
+.title__home {
+  font-size: 2rem;
+  text-align: center;
+  margin: 4rem 0 6rem;
+  font-weight: 300;
+  width: 65%;
+  line-height: 1.5;
+  font-family: "Inter", sans-serif;
+}
+
+.educational-sections-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: start;
+  margin: 0;
+}
+
+.educational-sections-wrapper p {
+  width: 32%;
+  font-size: 1.5rem;
+  margin: 0 8.3rem;
+}
+
+.color-ocean {
+  color: #3a7ca3;
+}
+.color-jungle {
+  color: #25a37e;
+}
+.color-forest {
+  color: #bdd526;
+}
+.color-desert {
+  color: #fdae84;
+}
+.color-arctic {
+  color: #abcdff;
+}
+
+.play-button-wrapper {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+}
+
+.cards-container {
+  display: flex;
+  flex-wrap: wrap; /* Permet aux cartes de passer à la ligne suivante si l'espace horizontal est insuffisant */
+  gap: 2rem; /* Espacement entre les cartes */
+  margin: 3rem auto; /* Marge supérieure et centrage horizontal */
+  justify-content: center; /* Centre les cartes horizontalement */
+  max-width: 1200px; /* Ajustez selon la largeur maximale souhaitée pour le conteneur */
+}
+
+.card-color-ocean {
+  background-color: #3574d5; /* Couleur bleue ajustée */
+}
+
+.card-color-jungle {
+  background-color: #25a37e; /* Couleur verte, si besoin d'ajustement */
+}
+
+.card-color-desert {
+  background-color: #fdae84; /* Couleur orange ajustée */
+}
+
+.card-color-arctic {
+  background-color: #d1e2fd; /* Couleur bleue ajustée */
+}
+</style>
